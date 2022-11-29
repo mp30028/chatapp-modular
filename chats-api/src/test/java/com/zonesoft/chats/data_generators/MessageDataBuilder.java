79e5f4a6fd776783f6d26033020c57f0fb4fdb5a
mp30028 <mp30028@gmail.com>
2022-11-29 04:43:51 +0000

    remove reference to conversation-id

--- chats-api/src/test/java/com/zonesoft/chats/data_generators/MessageDataBuilder.java
+++ chats-api/src/test/java/com/zonesoft/chats/data_generators/MessageDataBuilder.java
@@ -1,5 +1,11 @@
 package com.zonesoft.chats.data_generators;
 
+import java.time.OffsetDateTime;
+
 public class MessageDataBuilder {
+	private String id;
+	private String messageText;
+	private String senderParticipantId;
+	private OffsetDateTime sentTime;
 
 }
