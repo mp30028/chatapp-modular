146f146df3392bcde226e04fbbaee9a68e2002ad
mp30028 <mp30028@gmail.com>
2022-11-30 08:54:33 +0000

    understand mongo behaviour when collection doesn't exist or is empty

---
+++ tryouts/src/main/java/com/zonesoft/tryouts/models/Dummy.java
@@ -0,0 +1,90 @@
+package com.zonesoft.tryouts.models;
+
+import static com.zonesoft.utils.helpers.ToStringBuilder.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.zonesoft.utils.helpers.ToStringBuilder;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "dummies")
+public class Dummy {
+	@Id private String id;
+	private String moniker;
+	private String firstname;
+	private String lastname;
+	private List<OtherName> otherNames = new ArrayList<>();
+
+	public Dummy() {
+		super();
+	}
+
+	public Dummy(String moniker, String firstname, String lastname) {
+		super();
+		this.setMoniker(moniker);
+		this.setFirstname(firstname);
+		this.setLastname(lastname);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getMoniker() {
+		return moniker;
+	}
+
+	public void setMoniker(String moniker) {
+		this.moniker = moniker;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	
+	public List<OtherName> otherNames(){
+		return this.otherNames;
+	}
+	
+	List<OtherName> getOtherNames(){
+		return this.otherNames;
+	}
+	
+	void setOtherNames(List<OtherName> otherNames){
+		this.otherNames = otherNames;
+	}
+
+	@Override
+	public String toString() {		
+		return new ToStringBuilder()
+		.build(
+				lBrace, newline,
+					indent, key("id"), value(this.id), comma, newline,
+					indent, key("moniker"), value(this.moniker), comma, newline,
+					indent, key("firstname"), value(this.firstname), comma, newline,
+					indent, key("lastname"), value(this.lastname), comma, newline,
+					indent, key("other-names"), objectValue(this.otherNames), newline,
+				rBrace
+		);
+	}
+	
+}
