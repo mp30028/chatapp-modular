ab6d762493c3b8d96108944babaa686fbb36a2b3
mp30028 <mp30028@gmail.com>
2022-12-04 06:41:39 +0000

    created bootstrap class and test

---
+++ chats-ui/src/main/java/com/zonesoft/chatapp/ChatsUIApp.java
@@ -0,0 +1,13 @@
+package com.zonesoft.chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatsUIApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatsUIApp.class, args);
+	}
+
+}
---
+++ chats-ui/src/test/java/com/zonesoft/chatapp/ChatsUIAppTest.java
@@ -0,0 +1,14 @@
+package com.zonesoft.chatapp;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatsUIAppTest {
+	@Test
+	void contextLoads() {
+	}
+
+}
+
