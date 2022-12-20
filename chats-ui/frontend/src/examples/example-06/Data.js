5a9a9acc592281cf93f7ffbc6bc6f372720c8436
mp30028 <mp30028@gmail.com>
2022-12-20 16:34:04 +0000

    Factored out code to update persons data into seperarte component

--- chats-ui/frontend/src/examples/example-06/Data.js
+++ chats-ui/frontend/src/examples/example-06/Data.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect, useRef} from 'react';
 import * as FetchService from "./services/FetchService";
 import * as EventListenerService from "./services/EventListenerService";
+import * as PersonsDataService from "./services/PersonsDataService";
+
 function Data(){
 	const emptyPerson = {
 							"firstname": "",
@@ -17,58 +19,14 @@ function Data(){
 		personsRef.current = persons;
 	},[persons]);
 	
-	useEffect(() => {
-		const doSave = (currentData, eventData) =>{
-			var personToSave = null;
-			var existsInData = false;
-			var update = (item) => {	if(item.id === personToSave.id) {
-											existsInData = true; 
-											return personToSave; 
-										}else{
-											return item;
-										} 
-									};
-			for(let j = 0; j < eventData.persons.length; j++){
-				personToSave = eventData.persons[j];
-				existsInData = false;
-				var mappedPersons = currentData.map(update);	
-				if (!existsInData){
-					mappedPersons.push(personToSave);
-				}
-				currentData = mappedPersons;
-			}
-			return currentData;			
-		}
-		
-		const doDelete = (currentData, eventData) =>{
-			var personToDelete = null;
-			const toKeep = (item) => (item.id !== personToDelete.id);
-			for(let k =0; k < eventData.persons.length; k++){
-				personToDelete = eventData.persons[k];
-				var filteredPersons = currentData.filter(toKeep);
-				currentData = filteredPersons;
-			}
-			return currentData;	
-		}
-		
-		const updatePersons = (eventData) =>  {
-			var currentData = personsRef.current;
-			if (eventData.eventType === "SAVE"){
-				currentData = doSave(currentData, eventData);
-			}else if (eventData.eventType === "DELETE"){
-				currentData = doDelete(currentData, eventData);
-			}
-			setPersons(currentData);
-		};
-		
+	useEffect(() => {	
 		const onmessageHandler = (event) =>{
 			var eventData = JSON.parse(event.data);
-			updatePersons(eventData);
+			var updatedData = PersonsDataService.update(personsRef.current, eventData);
+			setPersons(updatedData);
 		};
-		
 		FetchService.fetchPersons().then((data) => setPersons(data));;
-		EventListenerService.setupEventSource(onmessageHandler);
-		
+		EventListenerService.setupEventSource(onmessageHandler);		
 	}, [setPersons]);
 
 
