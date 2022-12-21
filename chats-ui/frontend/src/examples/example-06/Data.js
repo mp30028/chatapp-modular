7e7b8c0d637f469d7af4ef31ca3072769a2f0a98
mp30028 <mp30028@gmail.com>
2022-12-21 13:51:39 +0000

    Person list and editor synced and reactive display working in example-06

--- chats-ui/frontend/src/examples/example-06/Data.js
+++ chats-ui/frontend/src/examples/example-06/Data.js
@@ -1,95 +1,44 @@
-import React, {useState, useEffect, useRef} from 'react';
-import * as FetchService from "./services/FetchService";
-import * as EventListenerService from "./services/EventListenerService";
-import * as PersonsDataService from "./services/PersonsDataService";
+import React, { useState } from 'react';
+import PersonList from "./components/PersonList";
+import PersonEditor from "./components/PersonEditor";
 
 function Data(){
-	const emptyPerson = {
-							"firstname": "",
-							"id": "NOT-SELECTED",
-							"lastname": "",
-							"moniker": "",
-							"otherNames":[]
-						}
-	const personsRef = useRef([]);
-	const [persons, setPersons]= useState([]);
-	const [selectedPerson, setSelectedPerson]= useState(emptyPerson);
-		
-	useEffect(() =>{
-		personsRef.current = persons;
-	},[persons]);
 	
-	useEffect(() => {	
-		const onmessageHandler = (event) =>{
-			var eventData = JSON.parse(event.data);
-			var updatedData = PersonsDataService.update(personsRef.current, eventData);
-			setPersons(updatedData);
-		};
-		FetchService.fetchPersons().then((data) => setPersons(data));;
-		EventListenerService.setupEventSource(onmessageHandler);		
-	}, [setPersons]);
-
-
-	const personRowClicked = (selectedRow) => {
-		console.log(selectedRow.moniker);
-		setSelectedPerson(selectedRow);
-	}
+	const [selectedPerson, setSelectedPerson] = useState(null);
+	
+	const personSelectionHandler = (person) =>{
+//		if(person){
+//			alert(selectedPerson.moniker);
+			setSelectedPerson(person);
+//		}
 		
-		return(
-			<div style={{width: "100%"}}>
-				<table>
-					<thead>
-						<tr>
-							<th>ID</th>
-							<th>Moniker</th>
-							<th>Firstname</th>
-							<th>Lastname</th>
-							<th>Other-Names</th>
-						</tr>
-					</thead>
-					<tbody>
- 
-						<tr>
-							<td colSpan="2">
-								<table>
-									<tbody>
-										<tr><th>ID</th><td>{selectedPerson.id}</td></tr>
-										<tr><td>Moniker</td><td>{selectedPerson.moniker}</td></tr>
-										<tr><td>Firstname</td><td>{selectedPerson.firstname}</td></tr>
-										<tr><td>Lastname</td><td>{selectedPerson.lastname}</td></tr>
-										<tr><td>Other-Names</td><td>
-																	{selectedPerson.otherNames
-																		.map(o => 
-																			<div key={o.id}>
-																				{o.nameType + ": " + o.value}
-																			</div>
-																		)
-															  		}
-																</td>
-										</tr>
-									</tbody>
-								</table>
-							</td>
-						</tr>
+	}
+	
+	const addNew = (event) =>{
+		alert("Add New Clicked");
+	};
 
-						{persons.map(p =>
-							<tr key={p.id} onClick={()=> personRowClicked(p)}>
-								<td style={{ width: "10%" }}>{p.id}</td>
-								<td style={{ width: "10%" }}>{p.moniker}</td>
-								<td style={{ width: "10%" }}>{p.firstname}</td>
-								<td style={{ width: "10%" }}>{p.lastname}</td>
-								<td style={{ width: "10%" }}>{p.otherNames.map(o => 
-																					<div key={o.id}>
-																						{o.nameType + ": " + o.value}
-																					</div>
-																				)
-															  }
-								</td>
-							</tr>
-						)}
-					</tbody>
-				</table>
-			</div>
+
+		return(
+			<table style={{ width: "100%"}}>
+				<tbody>
+					<tr>
+						<td style={{width: "70%", borderWidth: "0"}} >
+							<PersonList selectionHandler={personSelectionHandler} />
+						</td>
+						<td style={{verticalAlign: "top", borderWidth: "0"}} >
+							<PersonEditor person={selectedPerson} />
+						</td>
+					</tr>
+					<tr>
+						<td colSpan="2">
+							<div style={{ textAlign: "right" }}>
+								<button type="button" onClick={addNew}>New</button>
+							</div>
+						</td>
+					</tr>
+				</tbody>
+			</table>
 		);
 };
 
---
+++ chats-ui/frontend/src/examples/example-06/components/PersonEditor.js
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from 'react';
+import OtherNamesEditor from './OtherNamesEditor';
+
+export function Editor(props) {
+	const emptyPerson = {
+							"firstname": "",
+							"id": "",
+							"lastname": "",
+							"moniker": "",
+							"otherNames":[]
+						}
+
+	const [currentPerson, setCurrentPerson] = useState(emptyPerson);
+
+	useEffect(() =>{
+		setCurrentPerson(props.person ? props.person : emptyPerson);
+	},[props.person]);
+	
+	const handleChange = (event) => {
+//		const {name, value} = event.target;
+//		if (name === 'id'){
+//			setCurrentPerson({...currentPerson, [name]:parseInt(value)})	
+//		}else{
+//			setCurrentPerson({...currentPerson, [name]:value})
+//		}
+//		
+	}
+	
+	const handleSubmit = (event) =>{
+		event.preventDefault();
+		switch(event.target.value){
+			case "UPDATE":
+//				props.updatePerson(currentPerson);
+				break;
+			case "CREATE":
+//				props.addNewPerson(currentPerson);
+				break;
+			case "CANCEL":
+//				navigate("/list", { replace: true });
+				break;
+			case "DELETE":
+//				props.deletePerson(currentPerson);
+				break;
+			default:
+		}
+	}
+
+
+	
+	return (
+		<form style={{width:"100%" }}>
+			<table  className="zsft-table" style={{width:"100%" }}>
+				<tbody>
+					<tr>
+						<th>Person ID</th>
+						<td>
+							<input type="text" name="personId" id="personId" value={currentPerson.id} readOnly style={{width:"90%" }} />
+						</td>
+					</tr>
+					<tr>
+						<th>Moniker</th>
+						<td><input type="text" name="moniker" id="moniker" value={currentPerson.moniker} onChange={handleChange} /></td>
+					</tr>
+					<tr>
+						<th>Firstname</th>
+						<td><input type="text" name="firstname" id="firstname" value={currentPerson.firstname} onChange={handleChange} /></td>
+					</tr>
+					<tr>
+						<th>Lastname</th>
+						<td><input type="text" name="lastname" id="lastname" value={currentPerson.lastname} onChange={handleChange} /></td>
+					</tr>	
+					<tr>
+						<th>Other Names</th>
+						<td className="subtableContainer">
+							<OtherNamesEditor otherNames={currentPerson.otherNames}/>
+						</td>
+					</tr>
+					<tr>
+						<td colSpan="2" style={{textAlign:"right"}}>
+							<button type="submit" onClick={handleSubmit} value="DELETE">Delete</button>
+							<button type="submit" onClick={handleSubmit} value="UPDATE">Save</button>					
+							<button type="submit" onClick={handleSubmit} value="CANCEL">Cancel</button>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</form>
+
+
+	);
+
+}
+
+export default Editor;
