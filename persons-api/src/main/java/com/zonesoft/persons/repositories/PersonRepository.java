8c82f14b5c4ac3eeab9e998c9e99a51e17a1f016
mp30028 <mp30028@gmail.com>
2023-01-22 07:04:15 +0000

    Extended persons repository to find persons by list of ids

--- persons-api/src/main/java/com/zonesoft/persons/repositories/PersonRepository.java
+++ persons-api/src/main/java/com/zonesoft/persons/repositories/PersonRepository.java
@@ -1,6 +1,8 @@
 package com.zonesoft.persons.repositories;
 
 
+import java.util.List;
+
 //import org.springframework.data.mongodb.repository.Query;
 //import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
@@ -15,6 +17,8 @@ import reactor.core.publisher.Flux;
 public interface PersonRepository extends ReactiveMongoRepository<Person, String> {
 	
 	public Flux<Person> findByMoniker(String moniker);
+	
+	public Flux<Person> findByIdIn(List<String> ids);
 	
 //	  @Tailable
 //	  @Query("{}")
