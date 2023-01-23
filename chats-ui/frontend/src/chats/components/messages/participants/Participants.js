5902c816df9cb51da4af9c75ac7261b42534aeca
mp30028 <mp30028@gmail.com>
2023-01-23 05:19:00 +0000

    display participants

--- chats-ui/frontend/src/chats/components/messages/participants/Participants.js
+++ chats-ui/frontend/src/chats/components/messages/participants/Participants.js
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from "react";
 import * as DataService from "../../../services/ConversationDataService"
 
 function Participants(props){
+	const emptyParticipant =   {id: "",moniker: "",firstname: "",lastname: "",otherNames: []};
 	const emptyParticipants = [];
 	const [participants, setParticipants] = useState(emptyParticipants);
+	const [selectedParticipant, setSelectedParticipant]= useState(emptyParticipant);
 	
 	const enrichAndSetParticipants = (inParticipants) => {
 		if(Array.isArray(inParticipants)){
@@ -17,6 +19,7 @@ function Participants(props){
 	
 	useEffect(() =>{
 		enrichAndSetParticipants(props.participants);
+		setSelectedParticipant(emptyParticipant);
 	},[props.participants]);	
 	
 	
@@ -26,12 +29,49 @@ function Participants(props){
 		(contentElement.style.display === "block") ? contentElement.style.display = "none": contentElement.style.display = "block"; 
 	};
 	
+	const selectParticipant = (event) => {
+		const targetParticipant = participants.find(p => p.id === event.target.value);
+		if (targetParticipant){
+			setSelectedParticipant(targetParticipant);	
+		}else{
+			setSelectedParticipant(null);
+		}	
+	}
+	
+	const isChecked = (targetParticipantId) =>{
+		return ( selectedParticipant ? selectedParticipant.id === targetParticipantId : false);
+	}
+	
 	return(
 		<main>
 			<div className="collapsible" onClick={toggleCollapsible}> <h2 style={{display:"inline"}}>Participants</h2> (click to see details)</div> 
+			
+			<table className="zsft-table" style={{display:"none"}}>
+				<thead>
+					<tr>
+						<th>Moniker</th>
+						<th>--</th>
+					</tr>
+				</thead>
+					<tbody>
+						{participants.map(p =>
+							<tr key={p.id}>
+								<td>{p.moniker}</td>
+								<td>
+									<input type="radio" name={"selectParticipant_" + p.id} id={"selectParticipant_" + p.id} value={p.id} onChange={selectParticipant} checked={isChecked(p.id)} />
+									<label htmlFor={"selectParticipant_" + p.id} className="ellipses">...</label>
+								</td>
+							</tr>
+						)}
+					</tbody>
+			</table>
+			
+{/* 		
+			
 			<pre className="collapsibleContent">
 				{JSON.stringify(participants, null, 2)}
 			</pre>
+*/}	
 		</main>
 	);
 };
